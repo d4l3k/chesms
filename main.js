9a521f253dbc7a7f24f461ba581b8938455cfd0b
David Baldwynn <david.baldwin@alumni.ubc.ca>
2015-10-18 06:25:25 -0700

    added console.log

--- main.js
+++ main.js
@@ -130,7 +130,7 @@ app.post('/sms/reply/', function (req, res) {
               //TODO: need to do proper validation
               if(smsBody.length > 4){
 
-                console.log(smsBoy.slice(2,3));
+                console.log(smsBody.slice(2,3));
                 //If we have a move command
                 if(smsBody.slice(2,3) === 'to'){
                   var piece_pos = smsBody.slice(0,1);
