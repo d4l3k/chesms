0d518c53bdb2f01bc975a4c9421ca361b9bb444b
Tristan Rice <rice@fn.lc>
2015-10-18 19:15:32 +0000

    Changed lowercasing

--- main.js
+++ main.js
@@ -53,138 +53,136 @@ twilio.sendMessage({
 var phoneNumbers = {};
 
 var games = {};
-app.post('/sms/reply/', function (req, res) {
-  var body = '';
+app.post('/sms/reply/', function(req, res) {
+    var body = '';
 
-  req.on('data', function (data) {
-    body += data;
-  });
+    req.on('data', function(data) {
+        body += data;
+    });
 
-  req.on('end', function () {
+    req.on('end', function() {
 
-    var POST = qs.parse(body);
+        var POST = qs.parse(body);
 
-    console.log('text body', POST);
+        console.log('text body', POST);
 
-    //validate incoming request is from twilio using your auth token and the header from Twilio
-    var token = process.env.AUTH_TOKEN,
-    header = req.headers['x-twilio-signature'];
+        //validate incoming request is from twilio using your auth token and the header from Twilio
+        var token = process.env.AUTH_TOKEN,
+            header = req.headers['x-twilio-signature'];
 
-    //validateRequest returns true if the request originated from Twilio
-    if (twilio.validateRequest(token, header, 'http://nicki.fn.lc:8183/sms/reply/', POST)) {
+        //validateRequest returns true if the request originated from Twilio
+        if (twilio.validateRequest(token, header, 'http://nicki.fn.lc:8183/sms/reply/', POST)) {
 
-        //generate a TwiML response
-        var resp = new twilio.TwimlResponse();
-        var respMessage = '';
+            //generate a TwiML response
+            var resp = new twilio.TwimlResponse();
+            var respMessage = '';
 
-        if(!phoneNumbers.hasOwnProperty(POST.To)){
-          phoneNumbers[POST.To] = {
-            gameId: uuid.v1(),
-          };
+            if (!phoneNumbers.hasOwnProperty(POST.To)) {
+                phoneNumbers[POST.To] = {
+                    gameId: uuid.v1(),
+                };
 
-          var currGameId = phoneNumbers[POST.To].gameId;
+                var currGameId = phoneNumbers[POST.To].gameId;
 
-          //create new game
-          games[currGameId] = {
-            board: chess.create(),
-            players: [],
-          };
-          games[currGameId].players.push(POST.To);
+                //create new game
+                games[currGameId] = {
+                    board: chess.create(),
+                    players: [],
+                };
+                games[currGameId].players.push(POST.To);
 
-          respMessage = 'Add your friends phone # to start a game';
+                respMessage = 'Add your friends phone # to start a game';
 
-        }
+            } else {
+                var currGame = games[phoneNumbers[POST.To].gameId];
+                var smsBody = POST.Body.replace(/^\s+|\s+$/g, '');
 
-        else{
-          var currGame = games[phoneNumbers[POST.To].gameId];
-          var smsBody = POST.Body.replace(/^\s+|\s+$/g, '').toLowerCase();
+                //Check if we should add opponent or not
+                if (currGame.players.length === 1) {
 
-          //Check if we should add opponent or not
-          if(currGame.players.length === 1){
-
-            //Add person
-            if(smsBody.length === 10){
-              currGame.players.push(smsBody);
-              respMessage = 'Opponent with # '+smsBody+' successfully added';
-            }else {
-              respMessage = 'incorrect number. Try adding a correct friends #';
-            }
-          }
-          //Play Chess Game
-          else{
-            //If we lose or stalemate
-            if(currGame.board.isCheckmate || currGame.board.isStalemate){
-              //remove game
-              delete game[phoneNumbers[POST.To].gameId];
-
-              if(currGame.board.isCheckmate){
-                respMessage = 'Checkmate. Game Over'
-              }else {
-                respMessage = 'Stalemate. Gameover'
-              }
-            }
-            //If we aren't losing
-            else{
-
-              //if we have a valid message
-              //TODO: need to do proper validation
-              if(smsBody.length >= 4){
-
-                console.log(smsBody);
-                //If we have a move command
-                if(smsBody.split(' ')[0] === 'move'){
-                  // var piece_pos = smsBody.split(' ')[0];
-
-                  var move_pos = smsBody.split(' ')[1];
-
-                    //Error message
-                    try{
-                      // currGame.board.move(piece_pos);
-                      currGame.board.move(move_pos);
-                    }catch(err){
-                      console.log(err);
-                      resp.message('Error: '+err);
-                      res.writeHead(200, { 'Content-Type':'text/xml' });
-                      res.end(resp.toString());
-                      return;
+                    //Add person
+                    if (smsBody.length === 10) {
+                        currGame.players.push(smsBody);
+                        respMessage = 'Opponent with # ' + smsBody + ' successfully added\n' + renderBoard(currGame.board.getStatus());
+                    } else {
+                        respMessage = 'incorrect number. Try adding a correct friends #';
                     }
-                    console.log('piece moved');
-                    respMessage = 'Piece moved';
-                }else if(smsBody.split(' ')[0] === 'undo'){
-                  currGame.board.undo();
-                  respMessage = 'Move successfully undo';
-                }else if(smsBody.split(' ')[0] === 'moves'){
-
-                  console.log(currGame.board.getStatus().notatedMoves);
-                  // currGame.board.getStatus().notatedMoves.forEach(function(elem){
-                  //   respMessage += elem;
-                  //   respMessage += '\n';
-                  // });
                 }
-              }else{
+                //Play Chess Game
+                else {
+                    //If we lose or stalemate
+                    if (currGame.board.isCheckmate || currGame.board.isStalemate) {
+                        //remove game
+                        delete game[phoneNumbers[POST.To].gameId];
 
-                respMessage = 'Board\n' + renderBoard(currGame.board.getStatus());
-              }
+                        if (currGame.board.isCheckmate) {
+                            respMessage = 'Checkmate. Game Over'
+                        } else {
+                            respMessage = 'Stalemate. Gameover'
+                        }
+                    }
+                    //If we aren't losing
+                    else {
+
+                        //if we have a valid message
+                        //TODO: need to do proper validation
+                        if (smsBody.length >= 4) {
+
+                            console.log(smsBody);
+                            //If we have a move command
+                            var parts = smsBody.split(' ');
+                            var cmd = parts[0].toLowerCase();
+                            if (cmd === 'move') {
+
+                                var move_pos = parts[1];
+
+                                //Error message
+                                try {
+                                    currGame.board.move(move_pos);
+                                } catch (err) {
+                                    console.log(err);
+                                    resp.message('Error: ' + err);
+                                    res.writeHead(200, {
+                                        'Content-Type': 'text/xml'
+                                    });
+                                    res.end(resp.toString());
+                                    return;
+                                }
+                                console.log('piece moved');
+                                respMessage = renderBoard(currGame.board.getStatus());
+                            } else if (cmd === 'undo') {
+                                currGame.board.undo();
+                                respMessage = 'Move successfully undo';
+                            } else if (cmd === 'moves') {
+                                respMessage = Object.keys(currGame.board.getStatus().notatedMoves).join(' ');
+                            }
+                        } else {
+                            respMessage = renderBoard(currGame.board.getStatus());
+                        }
+                    }
+                }
+
             }
-          }
 
+
+            resp.message(respMessage);
+            //Write headers
+            res.writeHead(200, {
+                'Content-Type': 'text/xml'
+            });
+            res.end(resp.toString());
+        } else {
+            res.writeHead(403, {
+                'Content-Type': 'text/plain'
+            });
+            res.end('you are not twilio - take a hike.');
         }
-
-
-      resp.message(respMessage);
-      //Write headers
-      res.writeHead(200, { 'Content-Type':'text/xml' });
-      res.end(resp.toString());
-    }else {
-      res.writeHead(403, { 'Content-Type':'text/plain' });
-      res.end('you are not twilio - take a hike.');
-    }
-  });
+    });
 });
 
-var server = app.listen(8183, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+var server = app.listen(8183, function() {
+    var host = server.address().address;
+    var port = server.address().port;
 
-  console.log('CheSMS listening at http://%s:%s', host, port);
+    console.log('CheSMS listening at http://%s:%s', host, port);
 });
